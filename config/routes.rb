fa3104c321f4e630652c5ed601190f0b74a342ae
Waqas Anjum <Qanjum96@gmail.com>
2022-06-29 23:48:59 -0400

    done with lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # these are controllers
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  # route to test your configuration
+  get '/hello', to: 'application#hello_world'
 end
